80cef7ce73fc1976183c51cb7e62d40532e3eb0c
Art3mis15 <awesomeness.siew@gmail.com>
2025-01-20 23:22:23 -0800

    everything else in the game

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this)
         this.isFiring = false
         this.moveSpeed = 2
+
+        this.sfxShot = scene.sound.add('sfx-shot')
     }
 
     update() {
@@ -22,6 +24,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // fire button
         if(Phaser.Input.Keyboard.JustDown(keyFIRE)) {
             this.isFiring = true 
+            this.sfxShot.play()
         }
 
         // if fired. move up
@@ -34,6 +37,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false
             this.y = game.config.height - borderUISize - borderPadding
         }
+    }
+
+    reset() {
+        this.isFiring = false
+        this.y = game.config.height - borderUISize - borderPadding
     }
 }
 
