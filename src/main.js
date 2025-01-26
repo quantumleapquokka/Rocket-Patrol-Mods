fd70a348c90c35f948ef8b7bf8dfd87e60b7a072
Art3mis15 <awesomeness.siew@gmail.com>
2025-01-26 04:33:59 -0800

    test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+/**
+ * Name: Samantha Siew
+ * Title: Rocket Patrol Pro 
+ * Time:
+ * Mods:
+ *  1-Point Tier
+ *      Track a high score that persists across scenes and display it in the UI 
+ *      Create a new scrolling tile sprite for the background
+ * 
+ * 3-Point Tier
+ *      Display the time remaining on the screen
+ * 
+ * 5-Point Tier
+ *      Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+ *      Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses
+ *      Implement mouse control for player movement and left mouse click to fire
+ */
 let config = {
     type: Phaser.AUTO,
     width: 640,
